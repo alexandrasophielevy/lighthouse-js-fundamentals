304c734f9b953bd95b9e900cf34d1a6b6ff1c85a
Alexandra Levy <alexandrasophielevy@gmail.com>
2017-06-20 00:30:03 +0000

    Adds looping problems

---
+++ ingredients.js
@@ -0,0 +1,20 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+var i = 0
+while (var i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+for (var i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+function reverse(ingredients) {
+  var i = 0
+  var len = ingredients.length;
+  for (vari i = (len -1); i !== 0; i--) {
+    temp.push(a[i]);
+  }
+  return result;
+}
